cf7a9b446df8d6ef32149d8e4676dd21bb07df5e
Kass-Lopes <kasslopes0@gmail.com>
2026-07-08 19:36:12 +0100

    feat: adicionado filtro de user por email (FindByEmail)

--- src/infra/database/prisma/repositories/prisma-user-repository.ts
+++ src/infra/database/prisma/repositories/prisma-user-repository.ts
@@ -2,10 +2,12 @@ import { User } from 'src/modules/user/entities/User';
 import { UserRepository } from 'src/modules/user/repositories/user-repository';
 import { PrismaService } from '../prisma.service';
 import { Injectable } from '@nestjs/common';
+import { PrismaUserMappers } from '../mappers/prisma-user-mappers';
 
 @Injectable()
 export class PrismaUserRepository implements UserRepository {
   constructor(private readonly prisma: PrismaService) {}
+
   async create(user: User): Promise<void> {
     await this.prisma.user.create({
       data: {
@@ -16,5 +18,17 @@ export class PrismaUserRepository implements UserRepository {
         createdAt: user.createdat,
       },
     });
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      return null;
+    }
+    return PrismaUserMappers.toDomain(user);
   }
 }
