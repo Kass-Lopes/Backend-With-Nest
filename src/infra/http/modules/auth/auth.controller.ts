49ee3c3241983fe4cd7b39f719d3faff4a9dc699
Kass-Lopes <kasslopes0@gmail.com>
2026-07-09 22:34:20 +0100

    implementação do AuthGuard local

--- src/infra/http/modules/auth/auth.controller.ts
+++ src/infra/http/modules/auth/auth.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/modules/user/entities/User';
+//import { SigninBodyDTO } from './dtos/signin-body-dto';
 
 @Controller()
 export class AuthController {
   @Post('signIn')
   @HttpCode(HttpStatus.OK)
-  @UseGuards()
-  async signIn() {
-    await console.log();
+  @UseGuards(AuthGuard('local'))
+  signIn(@Request() request: User) {
+    return request.user as User;
   }
 }
