835785e3363d8f213ae1aa3b25a700444073ef8d
Kass-Lopes <kasslopes0@gmail.com>
2026-07-09 22:34:52 +0100

    Consumer para a roda 'signin'

--- src/infra/http/modules/auth/auth.module.ts
+++ src/infra/http/modules/auth/auth.module.ts
@@ -1,7 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { LocalStrategy } from 'src/modules/auth/strategies/local.strategy';
+import { ValidadeUserUseCase } from 'src/modules/auth/useCases/validateUserUseCase/validate-user-useCase';
+import { UserModule } from '../user/user.module';
+import { DatabaseModule } from 'src/infra/database/database.module';
+import { SigninValidateDToMiddleware } from './middleware/signin-validate-dto.middleware';
 
 @Module({
+  imports: [UserModule, DatabaseModule],
   controllers: [AuthController],
+  providers: [LocalStrategy, ValidadeUserUseCase],
 })
-export class AuthModule {}
+export class AuthModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(SigninValidateDToMiddleware).forRoutes('/signin');
+  }
+}
