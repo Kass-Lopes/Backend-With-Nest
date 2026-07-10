7ea3e966567fe00fbcc2a0c4dc440916784ecf38
Kass-Lopes <kasslopes0@gmail.com>
2026-07-10 21:20:38 +0100

    Autenticação JWT de forma global

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './infra/http/modules/user/user.module';
 import { DatabaseModule } from './infra/database/database.module';
 import { AuthModule } from './infra/http/modules/auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuards } from './infra/http/modules/auth/guards/jwt-auth.guards';
 
 @Module({
   imports: [DatabaseModule, UserModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuards,
+    },
+  ],
 })
 export class AppModule {}
