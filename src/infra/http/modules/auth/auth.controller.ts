c026caf3aa4e0629fa94ab50d0bb915650579315
Kass-Lopes <kasslopes0@gmail.com>
2026-07-10 21:22:37 +0100

    Local Auth para a roda de login que vai retornar o Token jwt

--- src/infra/http/modules/auth/auth.controller.ts
+++ src/infra/http/modules/auth/auth.controller.ts
@@ -1,21 +1,33 @@
 import {
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Post,
   Request,
   UseGuards,
 } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { User } from 'src/modules/user/entities/User';
-//import { SigninBodyDTO } from './dtos/signin-body-dto';
+//import { AuthGuard } from '@nestjs/passport';
+import { AuthRequestModels } from './models/auth-request-models';
+import { SigninUseCase } from 'src/modules/auth/useCases/signinUseCase/signin-useCase';
+import { LocalAuthGuards } from './guards/local-auth.guards';
+import { Public } from './decorators/isPublic';
 
 @Controller()
 export class AuthController {
+  constructor(private readonly signinUseCase: SigninUseCase) {}
+
   @Post('signIn')
+  @Public()
   @HttpCode(HttpStatus.OK)
-  @UseGuards(AuthGuard('local'))
-  signIn(@Request() request: User) {
-    return request.user as User;
+  @UseGuards(LocalAuthGuards)
+  signIn(@Request() request: AuthRequestModels) {
+    const access_token = this.signinUseCase.excute({ user: request.user });
+    return { access_token };
+  }
+
+  @Get('test')
+  test(@Request() request: AuthRequestModels) {
+    return request.user;
   }
 }
